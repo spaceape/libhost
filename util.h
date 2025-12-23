6bb1d18550b5ca271398427a140d8bb026f7f35a
mstanciu <spaceape99@gmail.com>
2025-12-23 08:56:37 +0200

    dev@251223

--- util.h
+++ util.h
@@ -1,6 +1,6 @@
 #ifndef core_util_h
 #define core_util_h
-/** 
+/**
     Copyright (c) 2021, wicked systems
     All rights reserved.
 
@@ -8,16 +8,16 @@
     conditions are met:
     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
       disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
       disclaimer in the documentation and/or other materials provided with the distribution.
     * Neither the name of wicked systems nor the names of its contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
 
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
     SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **/
@@ -59,9 +59,9 @@ constexpr float get_round_value(float value, float granularity) noexcept {
 /* get_div_lb()
  * get lower bound value result from the division of value with divider
 */
-constexpr int get_div_lb(int value, int divider) noexcept {
-      int l_div_value = value / divider;
-      return l_div_value;
+template<typename Xt>
+constexpr Xt get_div_lb(Xt value, Xt divider) noexcept {
+      return value / divider;
 }
 
 /* get_div_lb()
@@ -75,9 +75,10 @@ constexpr int get_div_lb(float value, float divider) noexcept {
 /* get_div_ub()
  * get upper bound value result from the division of value with divider
 */
-constexpr int get_div_ub(int value, int divider) noexcept {
-      int  l_div_value = value / divider;
-      int  l_mod_value = value % divider;
+template<typename Xt>
+constexpr Xt get_div_ub(Xt value, Xt divider) noexcept {
+      Xt  l_div_value = value / divider;
+      Xt  l_mod_value = value % divider;
       if(l_mod_value > 0) {
           return l_div_value + 1;
       } else
